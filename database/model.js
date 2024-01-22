9183eb20992a1d29a55e50d4c69c7fb1379d5b36
AdamSydnor <adamsydnor96@gmail.com>
2024-01-22 11:59:08 -0600

    user login

--- database/model.js
+++ database/model.js
@@ -3,7 +3,7 @@ import url from "url";
 import util from 'util';
 import connectToDB from "./db.js";
 
-const db = await connectToDB(`postgresql:///dndcreator`);
+const db = await connectToDB(`postgresql:///vibes`);
 
 // Table initializations:
 class User extends Model {
---
+++ server/controllers/authController.js
@@ -0,0 +1,58 @@
+import { User } from '../../database/model.js';
+
+async function checkStatus (req, res) {
+    if (req.session.user) {
+        let user = {...req.session.user};
+        delete user.password;
+        res.status(200).json({ user: req.session.user, loggedIn: true });
+        return;
+    } else {
+        res.status(200).json({ user: null, loggedIn: false });
+        return;
+    };
+};
+
+async function login (req, res) {
+    const { username, password } = req.body;
+    const user = await User.findOne({where: {username: username}});
+
+    if (user && user.password === password) {
+        req.session.user = user;
+        let userCopy = {...user.toJSON()};
+        delete userCopy.password;
+        res.json({success: true, user: userCopy});
+        return;
+    } else {
+        res.json({success: false});
+        return;
+    };
+};
+
+async function logout (req, res) {
+    req.session.destroy();
+    res.json({success: true});
+    return;
+};
+
+async function register (req, res) {
+    const { username, email, password } = req.body;
+    if (username && email && password) {
+        let user = await User.findOne({where: {username: username}});
+        if (user) {
+            res.status(401).json({error: 'User already exists'});
+            return;
+        } else {
+            user = await User.create({username, email, password});
+            req.session.user = user;
+            let userCopy = {...user.toJSON()};
+            delete userCopy.password;
+            res.status(200).json({success: true, user: userCopy});
+            return;
+        };
+    } else {
+        res.status(401).json({error: 'Information missing.'});
+        return;
+    };
+};
+
+export { checkStatus, login, logout, register };
