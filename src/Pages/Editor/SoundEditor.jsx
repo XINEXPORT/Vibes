3c050bfde4050fa2b2e03f9055a3ab03bdc72c60
Christine Hoang <christinehoangt@gmail.com>
2024-01-23 19:21:30 -0600

    sounds are displaying upon pressing buttons in RoomHeader.jsx

--- src/Pages/Editor/SoundEditor.jsx
+++ src/Pages/Editor/SoundEditor.jsx
@@ -1,28 +1,45 @@
 import {useState} from 'react'
-import { useLoaderData } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
-import axios from 'axios'
+import SoundAccordion from './SoundAccordion.jsx'
 import './SoundEditor.css'
 
-const SoundEditor = () =>{
-    // const {sounds} = useLoaderData();
-    const [activeIndex, setActiveIndex] = useState(null)
-    console.log(activeIndex)
+const SoundEditor = ({sounds}) =>{
+    console.log(sounds);
+    const dispatch = useDispatch();
+    const modal = useSelector(state => state.editorOne.modal)
+    const [isSound1ModalOpen, setIsSound1ModalOpen] = useState(false);
+    const [isSound2ModalOpen, setIsSound2ModalOpen] = useState(false);
+    const [isSound3ModalOpen, setIsSound3ModalOpen] = useState(false);
+    const [isSound4ModalOpen, setIsSound4ModalOpen] = useState(false);
 
-    // const SoundEditor = ({sound, type, users}) =>{
-    //     const [sound, setSound] = useState();
-    // }
+    const openSound1Modal = () =>{
+        setIsSound1ModalOpen(true); 
+    }
+    const openSound2Modal = () =>{
+        setIsSound1ModalOpen(true); 
+    }
+    const openSound3Modal = () =>{
+        setIsSound1ModalOpen(true); 
+    }
+    const openSound4Modal = () =>{
+        setIsSound1ModalOpen(true); 
+    }
 
     return (
         <div className = "SoundEditor">
-        <div className = "sound-modal">
-            <main>
-                <button>Sound 1</button>
-                <button>Sound 2</button>
-                <button>Sound 3</button>
-                <button>Sound 4</button>
-            </main>
-        </div>
+            <div>
+                <button onClick = {openSound1Modal}>Sound 1</button>
+                <button onClick = {openSound2Modal}>Sound 2</button>
+                <button onClick = {openSound3Modal}>Sound 3</button>
+                <button onClick = {openSound4Modal}>Sound 4</button>
+            </div>
+
+            {isSound1ModalOpen &&(
+                <div className="modal">
+                <SoundAccordion sounds = {sounds}/>
+                <button onClick= {()=> setIsSound1ModalOpen(false)}>Close</button>
+                </div>
+            )}
         </div>
     )
 }
