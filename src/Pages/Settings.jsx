d97e1b657ff0bda03461c970cecc8f54d7468558
Christine Hoang <christinehoangt@gmail.com>
2024-01-25 21:52:18 -0600

    settings modal and contact info included

--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -2,25 +2,25 @@ import './Settings.css'
 import {useState} from 'react'
 import { useDispatch, useSelector } from "react-redux";
 import {useLoaderData} from 'react-router-dom'
-import MySettings from '../Pages/MySettings.jsx'
 import axios from 'axios'
 
-const Settings = ()=>{
+const Settings = ({closeModal, username, password, email})=>{
     const user = useSelector(state => state.login.user);
 
 
     return(
-        <div>
-        <h1>User Settings</h1>
-        <MySettings
-        userId = {userInfo.userId}
-        email = {userInfo.email}
-        setEmail = {setEmail}
-        password = {userInfo.password}
-        setPassword = {setPassword}
-        //Password will be hidden or hashed
-        />
-        <div>Upload Your Sounds</div>
+        <div className = "modalBackground">
+            <div className = "modalContainer">
+                <button 
+                onClick={()=>closeModal(false)}> X </button>
+                <label className = "title">User Settings</label>         
+                <label className = "username">Username</label>
+                    <div className = "form">{username}</div>
+                <label className = "email">Email</label>
+                    <div className = "form">{email}</div>
+                <label className = "password">Password</label>
+                    <div className = "form">{password}</div>
+             </div>
         </div>
     )
 }
