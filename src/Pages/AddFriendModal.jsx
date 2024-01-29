c7725a9cf7c87db2f616a12c19d697514e82c1a4
Christine Hoang <christinehoangt@gmail.com>
2024-01-29 13:48:45 -0600

    friendmodal worked on

--- src/Pages/AddFriendModal.jsx
+++ src/Pages/AddFriendModal.jsx
@@ -1,19 +1,61 @@
 import './AddFriendModal.css'
+import axios from 'axios'
+import {  Form } from 'react-bootstrap'
+import { useState } from 'react';
+import { useSelector } from "react-redux";
 
-const AddFriendModal = ({searchInput, setSearchInput, friendReqList, setFriendReqList, userId, username})=>{
+const AddFriendModal = ({searchInput, 
+                        setSearchInput, 
+                        friendReqList, 
+                        setFriendReqList, 
+                        userId, 
+                        username, 
+                        riendRequestModalState, 
+                        setFriendRequestModalState
+                    })=> {
 
+const user = useSelector(state => state.login.user);
+const [selectedFriend, setSelectedFriend] = useState(null);
+const [friend, setFriend]=useState([])
 
-    return(
+   
+const handleFriendRequest = async () => {
+        const inputData = new FormData()
+        inputData.append('friendId', friendId)
+
+   try {
+        const {data} = await axios.post(`/api/request`, userId);
+        console.log(data);
+        } catch (error) {
+            console.log (error)("Error requesting friend:", error)
+        }
+    };
+
+    return (
         <div>
-        <label>Search Friends</label>
-        <input
-        placeholder="type friend name"
-        onChange = {()=>{}}
-        ></input>
-        <button>Send Friend Request</button>
-        <button>Cancel</button>
-        </div>
-    )
-}
+        <Form method = "POST" encType = 'multipart/form-data' className = "upload" >
+        <Form.Group controlId="friendRequest" className="mb-3"></Form.Group>
 
-export default AddFriendModal
+        <Form.Label className = "friend-request">Search For Friends</Form.Label>
+        <Form.Group controlId="friendId" className="mb-3">
+            <Form.Control
+                    type = "input"
+                    value = {friendId}
+                    onChange = {(e) => setSelectedFriend(e.target.value)}
+                    />
+        </Form.Group>
+            
+
+        <div className = "friendreq-modalContainer"
+        onClick={()=>setFriendRequestModalState(false)}> X 
+        </div>
+
+        <button className = "send-friendreq" onClick = {handleFriendRequest}>
+            Send Friend Request
+            </button>
+            </Form>
+        </div>
+    );
+};
+
+export default AddFriendModal;
