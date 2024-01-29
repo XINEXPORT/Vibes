e0d7be0a4e5afcbf1ad64b37a1416800ae204c11
AdamSydnor <adamsydnor96@gmail.com>
2024-01-29 09:57:10 -0600

    the elements should now be positioned properly

--- src/Pages/Room/RoomBackground.jsx
+++ src/Pages/Room/RoomBackground.jsx
@@ -3,7 +3,9 @@ import './RoomBackground.css';
 
 const RoomBackground = ()=>{
     return(
-        <div className="MainRoom"></div>
+        <div className="MainRoom">
+            <img src="https://www.popsci.com/uploads/2021/07/27/pawel-czerwinski-eybM9n4yrpE-unsplash-scaled-e1685635468228.jpg?auto=webp&optimize=high&width=1440" />
+        </div>
     );
 };
 
