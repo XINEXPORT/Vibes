848b35ddd34e5a028fbc53bf5a01ab407bfaa308
Christine Hoang <christinehoangt@gmail.com>
2024-01-29 15:09:22 -0600

    friend search is working in AddFriendModal

--- src/Pages/AddFriendModal.jsx
+++ src/Pages/AddFriendModal.jsx
@@ -1,7 +1,7 @@
 import './AddFriendModal.css'
 import axios from 'axios'
 import {  Form } from 'react-bootstrap'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector } from "react-redux";
 
 const AddFriendModal = ({searchInput, 
@@ -10,29 +10,58 @@ const AddFriendModal = ({searchInput,
                         setFriendReqList, 
                         userId, 
                         username, 
-                        riendRequestModalState, 
-                        setFriendRequestModalState
+                        friendRequestModalState, 
+                        setFriendRequestModalState,
+                        userSearch
                     })=> {
 
+console.log(userSearch)
 const user = useSelector(state => state.login.user);
 const [selectedFriend, setSelectedFriend] = useState(null);
-const [friend, setFriend]=useState([])
+const [friend, setFriend]=useState(null)
+const [input, setInput] = useState("");
+
+useEffect(()=>{
+    return ()=>{
+        const results = userSearch.filter((user)=>{
+            
+            const {username} = user
+            console.log(username)
+            return username.includes(input)
+        
+        })
+      console.log(results)
+      //add <button onClick = {handleFriendRequest(user.userId)}> Add Friend </buton>
+      //currently is crashing due to handler not working
+      const searchField = results.map((user)=>{
+            return(    
+            <div className= "friend-choice">
+                <p>{user.username}</p>
+                <button onClick = {""}>Add Friend</button>
+            </div>
+            )
+        })
+        setFriend(searchField) 
+    }
+   
+},[input])
 
    
-const handleFriendRequest = async () => {
-        const inputData = new FormData()
-        inputData.append('friendId', friendId)
+// const handleFriendRequest = async (requesteeId) => {
+//         const inputData = new FormData()
+//         inputData.append('requesteeId', requesteeId)
 
-   try {
-        const {data} = await axios.post(`/api/request`, userId);
-        console.log(data);
-        } catch (error) {
-            console.log (error)("Error requesting friend:", error)
-        }
-    };
+//    try {
+//         const {data} = await axios.post(`/api/request`, userId);
+
+//         } catch (error) {
+//             console.log (error)("Error requesting friend:", error)
+//         }
+//     };
+
 
     return (
-        <div>
+        <div className = "addfriendmodal">
         <Form method = "POST" encType = 'multipart/form-data' className = "upload" >
         <Form.Group controlId="friendRequest" className="mb-3"></Form.Group>
 
@@ -40,19 +69,19 @@ const handleFriendRequest = async () => {
         <Form.Group controlId="friendId" className="mb-3">
             <Form.Control
                     type = "input"
-                    value = {friendId}
-                    onChange = {(e) => setSelectedFriend(e.target.value)}
+                    onChange = {(e) => setInput(e.target.value)}
                     />
         </Form.Group>
+
+        <div className = "friendchoices">
+           {friend}
+        </div>
             
 
         <div className = "friendreq-modalContainer"
         onClick={()=>setFriendRequestModalState(false)}> X 
         </div>
-
-        <button className = "send-friendreq" onClick = {handleFriendRequest}>
-            Send Friend Request
-            </button>
+        
             </Form>
         </div>
     );
