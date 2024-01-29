13b359f966031d663b0945895a6cffcfbb29ddde
Christine Hoang <christinehoangt@gmail.com>
2024-01-29 11:51:58 -0600

    saving changes to Account Nav related to Friend Requests

---
+++ src/Pages/AddFriendModal.jsx
@@ -0,0 +1,14 @@
+import './AddFriendModal.css'
+
+const AddFriendModal = ({input, setInput, friendReqList, setFriendReqList})=>{
+
+
+    return(
+        <div>
+        <label>Search Friends</label>
+        <input>type friend name</input>
+        </div>
+    )
+}
+
+export default AddFriendModal
