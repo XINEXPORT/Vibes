976327c6ad9f2d9480dd836880fc4f7cf324c61f
Christine Hoang <christinehoangt@gmail.com>
2024-01-30 13:46:37 -0600

    moved all AccountNav components to the AccountNav folder

--- src/router.jsx
+++ src/router.jsx
@@ -6,7 +6,7 @@ import {
 } from 'react-router-dom';
 import App from './App.jsx';
 import Room from './Pages/Room/RoomBackground.jsx';
-import Settings from './Pages/Settings.jsx';
+import Settings from './Pages/AccountNav/Settings.jsx';
 import RoomHeader from './Pages/Room/RoomHeader.jsx';
 
 const router = createBrowserRouter(
