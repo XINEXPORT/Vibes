7eb788fc61c81759fe14086bf507e0f4f2eece0b
Christine Hoang <christinehoangt@gmail.com>
2024-02-07 15:11:53 -0600

    saving socket changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import AccountNav from './Pages/AccountNav/AccountNav.jsx';
 import RoomHeader from './Pages/Room/RoomHeader.jsx';
 import Room from './Pages/Room/RoomBackground.jsx';
 import LoginPage from './Login/LoginPage.jsx';
+import socketIO from 'socket.io-client';
 
+const socket = socketIO.connect('http://localhost:8000');
 
 function App() {
   return (
     <div>
-      <AccountNav/>
+      <AccountNav socket={socket}/>
       <LoginPage/>
-      <Outlet/>
+      <Outlet context={{socket}}/>
     </div>
   )
 }
--- src/Pages/Room/RoomHeader.jsx
+++ src/Pages/Room/RoomHeader.jsx
@@ -5,16 +5,14 @@ import Visualizer from './Visualizer.jsx';
 import Editor from '../Editor/SoundEditor.jsx';
 import axios from 'axios';
 import { useState, useRef, useEffect } from 'react';
-import { useLoaderData, useNavigate } from 'react-router-dom';
+import { useLoaderData, useNavigate, useOutletContext } from 'react-router-dom';
 import SoundEditor from '../Editor/SoundEditor.jsx';
 import { CiPlay1, CiPause1 } from "react-icons/ci";
 import { RxReset } from "react-icons/rx";
 import { useSelector } from "react-redux";
-import socketIO from 'socket.io-client';
-
-const socket = socketIO.connect('http://localhost:8000');
 
 const RoomHeader = () => {
+    const {socket} = useOutletContext()
     const {sounds, favs, params} = useLoaderData();
     const user = useSelector(state => state.login.user);
     const navigate = useNavigate();
